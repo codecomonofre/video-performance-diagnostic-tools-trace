14a0f847c27ca4e1b2db1c95f1bef48f9cd0e683
onofre.junior <onofre.junior@gmail.com>
2024-04-11 17:53:06 -0300

    chore: Added docke file

---
+++ src/Benchmark/Program.cs
@@ -0,0 +1,43 @@
+﻿using Benchmark.Services;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using OrderManagement.Api.Services;
+
+namespace Benchmark
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            BenchmarkRunner.Run(typeof(BenchmarkOrderService));
+        }
+    }
+}
+
+namespace Benchmark.Services
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchmarkOrderService
+    {
+        private readonly IOrderService _service;
+
+        public BenchmarkOrderService()
+        {
+            _service = new OrderService();
+        }
+
+        [Benchmark]
+        public void TestGetOrdersSlowAsync()
+        {
+            _service.GetOrdersSlow();
+        }
+
+        [Benchmark]
+        public void TestGetOrdersFastAsync()
+        {
+            _service.GetOrdersFast();
+        }
+    }
+}
